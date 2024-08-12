c617e0cf0b94dc6a8e8fa5ff68c9b71c07376429
iamasmit <ashmitchaudhary82@gmail.com>
2024-08-12 22:40:28 +0545

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; // Import the Auth facade
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // Attempt to authenticate the user
+        if (Auth::attempt($request->only('email', 'password'))) {
+            // Authentication was successful
+            // You can return a success message or token here
+            return response(['message' => 'Login successful'], 200);
+        }
+        $user = $request->user();
+        $user->tokens()->delete();
+        $token = $user->createToken('token')->plainTextToken;
+        return response()->json(['token' => $token], 200);
+
+        // Authentication failed
+        // return response(['message' => 'Invalid credentials'], 401);
+    }
+    public function Register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+            ]);
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password)
+            ]);
+            return response()->json(['message' => 'User created successfully'], 200);
+    }
+    
+}
