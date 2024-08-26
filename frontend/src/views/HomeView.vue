6f0c17f3c392749e52a94d771c9e888338b40e8c
iamasmit <ashmitchaudhary82@gmail.com>
2024-08-26 20:39:44 +0545

    Tailwind css added

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+import Button from "primevue/button";
+
+import InputText from "primevue/inputtext";
+</script>
 
 <template>
     <main>
-        <h1>Hello World</h1>
+        <h1 class="text-2xl text-red-500">Hello World</h1>
+        <Button label="Submit" />
+        <InputText type="text" v-model="value" />
     </main>
 </template>
